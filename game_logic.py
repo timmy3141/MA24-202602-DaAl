d9e6399ae0556f0596329e3ccd95c82a06d03635
AleksMJ <167433905+aleks0mj@users.noreply.github.com>
2026-03-05 15:28:15 +0100

    Début de game_logic.py

--- game_logic.py
+++ game_logic.py
@@ -1,3 +1,13 @@
 class GameLogic:
+
     def __init__(self):
-        pass
+
+        self.board = [[0 for _ in range(8)] for _ in range(8)]
+
+        self.board[3][3] = 2
+        self.board[3][4] = 1
+        self.board[4][3] = 1
+        self.board[4][4] = 2
+
+    def play_move(self, row, col):
+        print(row,";",col)
